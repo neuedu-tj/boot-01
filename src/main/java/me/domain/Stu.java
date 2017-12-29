7a456b848cfef830f6d2b41403dd27ed086e1606
neuedu-tj <13752284294@qq.com>
2017-12-30 00:22:07 +0800

    add plugIn (lombok)

--- src/main/java/me/domain/Stu.java
+++ src/main/java/me/domain/Stu.java
@@ -1,12 +1,15 @@
 package me.domain;
 
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.Min;
 
 @Entity
+@Data
 public class Stu {
 
     @Id
@@ -18,37 +21,7 @@ public class Stu {
     private Integer age;
     private double score;
 
-    public Integer getSid() {
-        return sid;
-    }
-
-    public void setSid(Integer sid) {
-        this.sid = sid;
-    }
-
-    public String getSname() {
-        return sname;
-    }
-
-    public void setSname(String sname) {
-        this.sname = sname;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public double getScore() {
-        return score;
-    }
-
-    public void setScore(double score) {
-        this.score = score;
-    }
+    
 
     @Override
     public String toString() {
